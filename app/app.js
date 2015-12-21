538b62f5f20157d7b178a0b6b7a5714cef110e80
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-12-21 17:56:51 -0500

    Proyect created

---
+++ app/app.js
@@ -0,0 +1,7 @@
+var app = angular.module("app",["ngMaterial"]).config(function($mdThemingProvider){
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('indigo')
+      .accentPalette('cyan');
+
+});
